1bafbad0122adf7cb1bfec31cac78b0c2262d5c3
kimyk0101 <kimyk7131@gmail.com>
2024-12-18 12:51:15 +0900

    Update App.jsx

--- src/08-create/App.jsx
+++ src/08-create/App.jsx
@@ -54,16 +54,49 @@ function Article(props) {
   );
 }
 
+//  Create 컴포넌트
+function Create({ onCreate }) {
+  //  생성 함수 전달 (onCreate)
+  return (
+    <article>
+      <h2>Create</h2>
+      <form
+        onSubmit={(event) => {
+          //  폼 전송 이벤트
+          event.preventDefault(); //  기본 동작 중지
+          const title = event.target.title.value;
+          const body = event.target.body.value;
+          onCreate(title, body);
+        }}
+      >
+        <p>
+          <input type="text" name="title" placeholder="title" />
+        </p>
+        <p>
+          <textarea name="body" placeholder="body"></textarea>
+        </p>
+        <p>
+          <input type="submit" value="Create"></input>
+        </p>
+      </form>
+    </article>
+  );
+}
+
 function App() {
   const [mode, setMode] = useState("WELCOME");
   const [id, setId] = useState(null);
+  const [nextId, setNextId] = useState(4);
 
-  const topics = [
+  //   const topics = [
+  const [topics, setTopics] = useState([
     { id: 1, title: "html", body: "html is ..." },
     { id: 2, title: "css", body: "css is ..." },
     { id: 3, title: "javascript", body: "javascript is ..." },
-  ];
+  ]);
   let content = null;
+
+  //    모드 전환
   if (mode === "WELCOME") {
     content = <Article title="Welcome" body="Hello, Web"></Article>;
   } else if (mode === "READ") {
@@ -77,6 +110,32 @@ function App() {
       }
     }
     content = <Article title={title} body={body}></Article>;
+  } else if (mode === "CREATE") {
+    content = (
+      <Create
+        onCreate={(_title, _body) => {
+          //  새 topic 생성
+          const newTopic = { id: nextId, title: _title, body: _body };
+
+          //  topics 배열에  newTopic 추가
+
+          /*
+          topics.push(newTopic);
+          위 방법은 state 변화를 감지하지 못하고, 랜더링 또한 일어나지 않음
+          그래서 state를 변경할 때는 반드시 상태변경 함수를 활용해야 함(매우 중요)
+          또한 위 방법은 내부 데이터의 불변성을 해치게 되므로 쓰지 말 것
+          */
+
+          setTopics([...topics, newTopic]);
+          //    READ 모드로 이동
+          //    선택된 article id, mode -> READ
+          setMode("READ");
+          setId(nextId);
+          //    nextId 갱신
+          setNextId(nextId + 1);
+        }}
+      ></Create>
+    );
   }
   return (
     <div className="App">
