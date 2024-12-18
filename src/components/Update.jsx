ba30fc61b9bdb686aecbc851c76a847971704e29
kimyk0101 <kimyk7131@gmail.com>
2024-12-18 15:41:44 +0900

    Create Update.jsx

---
+++ src/components/Update.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+function Update(props) {
+  //  title, body, onUpdate
+  //   const title = props.title;
+  //   const body = props.body;
+  const [title, setTitle] = useState(props.title);
+  const [body, setBody] = useState(props.body);
+
+  return (
+    <article>
+      <h2>Update</h2>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          const title = event.target.title.value;
+          const body = event.target.body.value;
+          props.onUpdate(title, body);
+        }}
+      >
+        <p>
+          <input
+            type="text"
+            name="title"
+            placeholder="title"
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+          />
+        </p>
+        <p>
+          <textarea
+            name="body"
+            placeholder="body"
+            value={body}
+            onChange={(event) => setBody(event.target.value)}
+          ></textarea>
+          {/* value 속성과 onChange 이벤트를 이용, 상태를 관리 */}
+        </p>
+        <p>
+          <input type="submit" value="Update"></input>
+        </p>
+      </form>
+    </article>
+  );
+}
+
+export default Update;
