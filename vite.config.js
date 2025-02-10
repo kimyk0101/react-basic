d0a9db01c2802c07d464e1d85fb8636a221dfb1d
kimyk0101 <kimyk7131@gmail.com>
2025-02-10 14:37:59 +0900

    Add Dockerfile for dev

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0', //  모든 네트워크 인터페이스에서 접근 허용
+    port: 5173,    //  서버 포트를 5173으로
+    strictPort: true, //  포트 충돌이 발생하면 에러 발생
+  },
   plugins: [react()],
 })
